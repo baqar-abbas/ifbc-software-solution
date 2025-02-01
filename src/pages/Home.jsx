e4ce5aeacad8417d75450cfb0340e105174ca1ae
baqar-abbas <baqar.abbas@hotmail.com>
2025-02-01 20:45:36 +0500

    Create Tabs section for the Homepage along with the content.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Headline from "../components/HompageComponents/Headline/Headline";
+import TabsSection from "../components/HompageComponents/TabsSection/TabsSection";
 
 const Home = () => {
   return (
     <div>
       <Headline />
+      <TabsSection />
       <h1>This is Home page</h1>
     </div>
   );
